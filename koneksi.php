02af4a454b91bbd8b1bed595b692976d9445257e
Rengga Prakoso Nugroho <rengga.prakoso@gmail.com>
2021-07-29 23:52:58 +0700

    refactor: directory simplify

--- koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('HOST','localhost');
-define('USER','asauruse_user');
-define('PASS','&SI4UCqzOAO.');
-define('DB', 'asaurus_form');
- 
-// Buat Koneksinya
-$db1 = new mysqli(HOST, USER, PASS, DB);
-?>
