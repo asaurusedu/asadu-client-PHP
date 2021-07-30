9ad0759cf1d98bba31979bddbb66c6b716d5824f
Rengga Prakoso Nugroho <rengga.prakoso.2001216@students.um.ac.id>
2021-07-30 14:47:59 +0700

    build: changes MySQL Credentials to view only auth

--- src/koneksi.php
+++ src/koneksi.php
@@ -1,9 +1,9 @@
 <?php
 
 define("HOST", "localhost");
-define("USER", "asauruse_user");
-define("PASS", "&SI4UCqzOAO.");
-define("DB", "asaurus_form");
+define("USER", "asauruse_asaduclient");
+define("PASS", "ln54aJ)VI^B]");
+define("DB", "asauruse_form");
 
 // Buat Koneksinya
 $db1 = new mysqli(HOST, USER, PASS, DB);
