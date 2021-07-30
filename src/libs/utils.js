06aa648494c542bbacdf675bb3b2e71d81528390
Rengga Prakoso Nugroho <rengga.prakoso@gmail.com>
2021-07-30 21:25:34 +0700

    refactor: simplify formdata fetcher

--- src/libs/utils.js
+++ src/libs/utils.js
@@ -11,23 +11,56 @@ function getMateri() {
     cache: false,
     success: function (msg) {
       $("#materi").html(msg);
-      console.log(msg);
     },
   });
 }
 
+// function getFormData() {
+//   fetch("./data/query.json")
+//     .then((response) => response.json())
+//     .then((data) => {
+//       for (let current = 0; current < data.length; current++) {
+//         $.ajax({
+//           type: data[current].type,
+//           url: data[current].url,
+//           success: function (msg) {
+//             $("#" + data[current].target).html(msg);
+//             console.log(msg);
+//           },
+//         });
+//       }
+//     });
+// }
+
 function getFormData() {
-  fetch("./data/query.json")
-    .then((response) => response.json())
-    .then((data) => {
-      for (let current = 0; current < data.length; current++) {
-        $.ajax({
-          type: data[current].type,
-          url: data[current].url,
-          success: function (msg) {
-            $("#" + data[current].target).html(msg);
-          },
-        });
+  $.ajax({
+    type: "POST",
+    url: "get_formdata.php",
+    cache: false,
+    success: function (msg) {
+      let data = JSON.parse(msg);
+
+      // Add kelas data to form
+      for (let current = 0; current < data[0].length; current++) {
+        $("#kelas").append(
+          '<option value="' +
+            data[0][current].id_kelas +
+            '">' +
+            data[0][current].nama_kelas +
+            "</option>"
+        );
       }
-    });
+
+      // Add matpel data to form
+      for (let current = 0; current < data[1].length; current++) {
+        $("#matpel").append(
+          '<option value="' +
+            data[1][current].id_matpel +
+            '">' +
+            data[1][current].nama_matpel +
+            "</option>"
+        );
+      }
+    },
+  });
 }
